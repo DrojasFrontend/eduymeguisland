ca995c3e412a0c2445266b5bb6edc2d194f60f94
Daniel Rojas <danielrojas243@gmail.com>
2024-08-25 15:21:03 -0500

    update

--- template-parts/section-5.php
+++ template-parts/section-5.php
@@ -29,12 +29,12 @@ $placesCartagena = [
     [
         "img" => "cartagena-1.png",
         "name" => "Teatro Heredia",
-        "copy" => "Magnífico edificio de estilo ecléctico construido en 1902, diseñado por Luis Felipe Jaspe, el mismo arquitecto que creó la Torre del Reloj de Cartagena.",
+        "copy" => "Magníﬁco de estilo ecléctico diseñado por Luis Felipe Jaspe, el mismo arquitecto que creó la Torre del Reloj de Cartagena.",
     ],
     [
         "img" => "cartagena-2.png",
         "name" => "Murallas de Cartagena",
-        "copy" => "Fortificación que tardó más de cien años en completarse, llevada a cabo por etapas desde 1586 hasta 1699, abarcando el perímetro del centro de la ciudad vieja, San Diego y Getsemaní.",
+        "copy" => "Fortiﬁcación que tardó más de cien años en completarse, llevada a cabo por etapas desde 1586 hasta 1699, abarcando el perímetro del centro de la ciudad vieja, San Diego y Getsemaní.",
     ],
     [
         "img" => "cartagena-3.png",
@@ -42,40 +42,20 @@ $placesCartagena = [
         "copy" => "Famosa puerta de entrada a la ciudad amurallada, caracterizada por su portada barroca de piedra que precede a una imponente estructura.",
     ],
     [
-        "img" => "cartagena-4.png",
-        "name" => "Letrero de Cartagena",
-        "copy" => "El letrero está ubicado en la carretera que lleva a Bocagrande, el sector donde se encuentran la mayoría de los hoteles de la ciudad y donde hay bonitas playas públicas para nadar.",
-    ],
-    [
-        "img" => "cartagena-5.png",
-        "name" => "Barrio Getsemaní",
-        "copy" => "Es uno de los lugares más visitados de la ciudad. Aquí encontrarás grafitis, hoteles, cervecerías e increíble música que sin duda te harán volver a este lugar.",
-    ],
-    [
-        "img" => "cartagena-6.png",
-        "name" => "Convento Santa Cruz de la Popa",
-        "copy" => "El convento, claustro y capilla de Nuestra Señora de la Candelaria de la Popa se encuentran en la cima del Cerro de la Popa en Cartagena. Multitudes de devotos hacen una peregrinación a pie hasta la cima del cerro de la Popa.",
+        "img" => "cartagena-7.jpg",
+        "name" => "Isla de Barú",
+        "copy" => "Zona costera situada a 45 minutos en barco, conocida por sus paradisíacas playas casi vírgenes de arena blanca, aguas transparentes y arrecifes de coral.",
     ],
     [
         "img" => "cartagena-7.png",
         "name" => "Castillo San Felipe",
-        "copy" => "Uno de los sitios más famosos, corresponde a un castillo construido en el siglo XVII, obra de la ingeniería militar española en América.",
-    ],
-    [
-        "img" => "cartagena-8.png",
-        "name" => "Monumento de las Botas Viejas",
-        "copy" => "Ubicado en la base del Castillo de San Felipe, el Monumento a los Zapatos Viejos es una escultura gigante de un par de botas viejas. Un lugar popular para tomarse una selfie, el monumento fue creado por Héctor Lombana Piñeres.",
+        "copy" => "Uno de los emplazamientos más famosos, correspondiente a un castillo construido en el siglo XVII, considerado la obra más destacada de la ingeniería militar española en América.",
     ],
     [
         "img" => "cartagena-9.png",
-        "name" => "Islas del Rosario",
-        "copy" => "En las Islas del Rosario se puede disfrutar de un destino paradisíaco de hermosas playas de arena blanca y las cálidas y cristalinas aguas del Mar Caribe. (¡Estaremos en la Isla Barú el 25 de julio!)",
+        "name" => "Cerro de la Popa",
+        "copy" => "Punto más alto de Cartagena, una posición estratégica que permite unas vistas privilegiadas tanto de la ciudad vieja como del mar Caribe, la isla de Tierrabomba y La Boquilla.",
     ],
-    [
-        "img" => "cartagena-10.png",
-        "name" => "Volcán de Lodo del Totumo",
-        "copy" => "Es un cráter de 15 metros de altura con una piscina de lodo medicinal en la parte superior. Entrar en el cráter de lodo del volcán es toda una aventura, aunque será imposible hundirse en esta piscina natural que parece no tener fondo.",
-    ]
 ];
 ?>
 <section class="section-5" id="cartagena">
