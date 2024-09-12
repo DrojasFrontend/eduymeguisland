04afde7ed50d5c56dcf193c045417f02f459d0bc
DrojasFrontend <danielrojas243@gmail.com>
2024-09-12 16:21:35 -0500

    update

--- template-parts/section-6.php
+++ template-parts/section-6.php
@@ -5,9 +5,9 @@
             <div class="space space--20"></div>
             <p class="font--bosque heading--20 color--666 uppercase" style="letter-spacing: 4px;">CELEBRA COn NOSOTROS!</p>
             <div class="space space--30"></div>
-            <p class="font--bosque heading--14 color--333" style="-webkit-text-stroke-width: inherit;">Por favor conﬁrmar asistencia antes 11 de NOVIEMBRE del 2024</p>
+            <p class="font--bosque heading--14 color--333" style="-webkit-text-stroke-width: inherit;">Por favor conﬁrmar asistencia antes 1 de NOVIEMBRE del 2024</p>
             <div class="space space--10"></div>
-            <p class="font--bosque heading--14 color--333" style="-webkit-text-stroke-width: inherit;">Kindly respond by NOVEMBER 11th, 2024</p>
+            <p class="font--bosque heading--14 color--333" style="-webkit-text-stroke-width: inherit;">Kindly respond by NOVEMBER 1th, 2024</p>
             <div class="space space--50"></div>
         </div>
         <?php echo do_shortcode('[contact-form-7 id="c65ad8b" title="R.S.V.P"]')?>
